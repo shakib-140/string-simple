56ea781555af0c3e34289404e4ea59c0fb4a7d96
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2021-12-03 22:32:11 +0600

    Add files via upload

---
+++ string 1,c.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char str[50];
+    printf("enter your name =");
+    gets(str);
+    printf("your name is=%s",str);
+
+
+
+    getch();
+}
