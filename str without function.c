df06fd96d816bd27edd8392308bc5d4ee3f7250a
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2021-12-04 01:56:02 +0600

    Add files via upload

---
+++ str without function.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char str[]="jfjfidhuiyiyiyhfuiyheuiyeiwyuiwryiwuyweuiyfioroerfor";
+    int i=0,len=0;
+    while(str[i]!='\0')
+    {
+        i++;
+        len++;
+    }
+    printf("the string length is =%d",len);
+
+
+    getch();
+}
