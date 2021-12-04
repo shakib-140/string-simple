a18215fbda072394f43d77f22ce1c40aa27d424a
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2021-12-04 23:47:33 +0600

    Add files via upload

---
+++ strrev.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char str[]="shakib khan";
+    printf("the main string is=%s\n",str);
+    strrev(str);
+    printf("the revers string ia=%s",str);
+
+
+
+
+    getch();
+}
