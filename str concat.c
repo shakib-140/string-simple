1b0c505486f1fdcf966be18180e825f36919e9cc
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2021-12-04 16:43:06 +0600

    Add files via upload

---
+++ str concat.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+
+{
+    char str1 []="my name is ";
+    char str2 []="shakib khan";
+    strcat(str1,str2);
+    printf("%s",str1);
+    getch();
+}
