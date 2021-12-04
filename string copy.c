7d5a2e0c586eca6d406a67479c580b9ac44ef208
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2021-12-04 16:33:12 +0600

    Add files via upload

---
+++ string copy.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char source[]="shakib khan";
+    char target[40];
+    strcpy(target,source);
+    printf("the source string is=%s\n",source);
+    printf("the target string is=%s\n",target);
+
+
+
+
+
+    getch();
+}
