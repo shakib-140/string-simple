a53fb9c7ae416673738a00b663c4cd6b0c0e91b9
Shakirrojjaman <95244399+shakib-140@users.noreply.github.com>
2021-12-06 15:39:50 +0600

    Add files via upload

---
+++ str upper lower.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char str[]="shaKIB";
+    strupr(str);
+    printf("the upper string is=%s\n",str);
+    strlwr(str);
+    printf("the lower string is=%s\n",str);
+
+
+
+
+
+
+    getch();
+}
